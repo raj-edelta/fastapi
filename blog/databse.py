7f35a8083fd64233cba1738bfaa85836677ab282
raj-edelta <raj.mangukiya@edeltaes.com>
2023-12-19 18:06:36 +0530

    first commit

--- blog/databse.py
+++ blog/databse.py
@@ -1,9 +1,12 @@
 from sqlalchemy import create_engine,Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
+from dotenv import load_dotenv
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:halfsquare@localhost/raj-fastapi"
+load_dotenv()
 
+SQLALCHEMY_DATABASE_URL = os.getenv('SQLALCHEMY_DATABASE_URL')
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
 )
